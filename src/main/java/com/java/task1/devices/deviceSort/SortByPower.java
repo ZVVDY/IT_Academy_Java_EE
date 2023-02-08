5e94fcd9eeb101d2d5cd327504e740fd6fc3720c
Vladimir <dholl_1987@mail.ru>
2023-02-08 23:52:54 +0300

    Java EE Task1

---
+++ src/main/java/com/java/task1/devices/deviceSort/SortByPower.java
@@ -0,0 +1,20 @@
+package com.java.task1.devices.deviceSort;
+
+
+import java.util.Comparator;
+
+/**
+ * Компаратор для сортировки
+ * по мощности(n/n+1).
+ *
+ */
+//public class SortByPower implements Comparator<IDevice> {
+//    @Override
+//    public int compare(IDevice o1, IDevice o2) {
+//        if (o1.getPower() > o2.getPower()) {
+//            return 1;
+//        } else if (o1.getPower() < o2.getPower()) {
+//            return -1;
+//        } else return 0;
+//    }
+//}
